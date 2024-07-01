841e6d0dda200e310219036ebe5f1559765f4df6
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2024-07-01 10:11:12 -0300

    Primeiro commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
-  },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Engenharia Da computação",
+    duration: "2023 - Present",
+    institution: "IESB Campus sul",
   }
+  // {
+  //   id: 2,
+  //   title: "Higher Secondary Certificate",
+  //   duration: "2018 - 2020",
+  //   institution: "Noakhali Islamia Kamil Madrasah",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Secondary School Certificate",
+  //   duration: "2008 - 2018",
+  //   institution: "Baitus Saif Islamia Madrasah",
+  // }
 ]
