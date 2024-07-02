4ded74c8ecd5650de46a40aeb2def2602cb3038f
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2024-07-02 14:55:09 -0300

    curriculo1

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Fullstack",
   description: "Meu nome é Mayron tenho 19 anos, estou cursando Engenharia da Computação no IESB asa sul, atualmente estou no 3º semestre da faculdade no periodo noturno, moro na Santa Maria e comcei minha caminhada no mundo da programação quando ingressei na faculdade e mesmo que sem querer acabei me apaixonando por esse mundo de T.I",
   leetcode: "https://leetcode.com",
-  resume: "https://drive.google.com/file/d/1l1rpVUfACe2fM1tl_QUReBnw222N_lND/view",
+  resume: "https://drive.google.com/file/d/1jRmsMNT8PQSYyypIZlanD1lzfaMN1Ot7/view",
   email: 'mayronmalaquias1607@gmail.com',
   phone: '(61) 98203-9522',
   address: 'Santa Maria - DF ',
